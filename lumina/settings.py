c9ad4f5410953321e588cfe1afab406c3c80ee53
rapido-pixel <68227960+rapido-pixel@users.noreply.github.com>
2020-10-14 00:20:27 +0300

    Update settings.py

--- lumina/settings.py
+++ lumina/settings.py
@@ -144,3 +144,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
