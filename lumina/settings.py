15b1e5f90f199f127310967d31ffc277a3257ce1
rapido-pixel <68227960+rapido-pixel@users.noreply.github.com>
2020-10-18 17:43:08 +0300

    Update settings.py

--- lumina/settings.py
+++ lumina/settings.py
@@ -161,4 +161,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)hgf
+DATABASES['default'].update(db_from_env)
